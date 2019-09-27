336ac98523f115ca45a6bc9039896b86ccf66048
sjuuun <osjuny11@gmail.com>
2019-09-27 23:38:30 +0900

    first complete hw1-1

--- hw1/hw1_1.py
+++ hw1/hw1_1.py
@@ -7,16 +7,40 @@ sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1])
 
 def divide(x):
-    for i in x[1]:
-        (i, x[0])
+	result = [];    
+	for i in x[1]:
+		result.append((i, x[0]));
+	return result
 
-words = lines.map(lambda l: re.split(r'\t', l)) \
+pairs = lines.map(lambda l: re.split(r'\t', l)) \
+        .filter(lambda l: len(l) > 1) \
         .map(lambda s: (s[0], re.split(r',', s[1]))) \
-        .map(lambda x: divide(x)) \
-        .collect()
+	.flatMap(divide)
 
-print "Hello worls\n"
+pair_list = pairs.collect()
 
-print words
+common = pairs.groupByKey() \
+         .mapValues(list)
+
+def make_interest(x):
+	people = x[1]
+	result = []
+	for i in range(len(people)):
+		for j in (range(i+1, len(people))):
+			result.append((people[i], people[j]))
+	return result	
+
+interest = common.flatMap(make_interest) \
+           .filter(lambda p: not (p in pair_list)) \
+           .map(lambda p: (p, 1)) \
+           .reduceByKey(lambda n1, n2: n1 + n2) \
+           .sortByKey()
+
+final = interest.map(lambda i: (i[1], i[0])) \
+        .sortByKey(False) \
+        .take(10)
+
+for i in final:
+	print "%s\t%s\t%d" % (i[1][0], i[1][1], i[0])
 
 sc.stop()
