45b7eb4039a29d37363065529cb876fc6ae28ef3
sjuuun <osjuny11@gmail.com>
2019-09-27 14:06:09 +0900

    test hw1_1

--- hw1/hw1_1.py
+++ hw1/hw1_1.py
@@ -6,28 +6,17 @@ conf = SparkConf()
 sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1])
 
-# split with space, make lower case, remove duplication, and filter non-word
-words = lines.flatMap(lambda l: re.split(r'[^\w]+', l)) \
-        .map(lambda w: w.lower()) \
-        .distinct() \
-        .filter(lambda chunk: len(chunk) > 0)
+def divide(x):
+    for i in x[1]:
+        (i, x[0])
 
-# Take first letter from words, and check it is alphabet
-first = words.map(lambda word: (word[0], 1)) \
-        .filter(lambda letter: letter[0].isalpha())
-
-# Count words according to first letter
-counts = first.reduceByKey(lambda n1, n2: n1 + n2) \
-        .sortByKey() \
+words = lines.map(lambda l: re.split(r'\t', l)) \
+        .map(lambda s: (s[0], re.split(r',', s[1]))) \
+        .map(lambda x: divide(x)) \
         .collect()
 
-# Print the result
-for index in range(26):
-    alpha = chr(ord('a')+index)
-    if index < len(counts) and alpha == counts[index][0]:
-        print "%s   %d" % (alpha, counts[index][1])
-    # If there isn't word starting with alpha, result is 0
-    else:
-        print "%s   %d" % (alpha, 0)
+print "Hello worls\n"
+
+print words
 
 sc.stop()
