25e63bffc33a4913697895acf429c3b21d8aaafc
sjuuun <osjuny11@gmail.com>
2019-10-02 03:02:23 +0900

    hw1_1 might be completed

--- hw1/hw1_1.py
+++ hw1/hw1_1.py
@@ -8,59 +8,34 @@ sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1])
 
 
-split = lines.map(lambda l: re.split('\t|,', l)) \
-        .collect()
-        #.collect()
-        #lines.map(lambda l: re.split(r'\t', l)) \
-        #.map(lambda s: (s[0], re.split(r',', s[1]))) \
-        #.collect()
-	    #.filter(lambda l: len(l) > 1) \
-        # #.filter(lambda s: len(s[1]) > 1) \
+split = lines.map(lambda l: re.split(r'\t|,', l)) \
+        .filter(lambda l: l[-1] != '')
+ 
+#print "This is split"
+#print split.collect()
 
-#numPeople = int(max(max(split))) + 1
-numPeople = 50000
-
-#print "How many people"
-#print numPeople
-
-# Make path(friends) matrix
-friendMat = np.zeros((numPeople, numPeople))
-
-for i in range(numPeople):
-    friends = split[i][1:]
-    for j in friends:
-        friendMat[i][int(j)] += 1
-
-#print "Matrix of friend"
-#print friendMat
-
-# make (index, value) pair
 def make_pair(x):
-    pairs = []
-    for i in range(len(x)):
-        pairs.append((i, int(x[i])))
-    return pairs
+    who = x[0]
+    friends = x[1:]
+    result = []
+    for i in range(len(friends)):
+        result.append(((who, friends[i]), 0))
+        for j in range(i+1, len(friends)):
+            result.append(((friends[i], friends[j]), 1))
+    return result
 
-# multiplication of symmetric matrix
-def matrix_multiple(x):
-    value = []
-    for i in x:
-        for j in x:
-            value.append(((i[0],j[0]), i[1]*j[1]))
-    return value
+def count_common(n1, n2):
+    if n1*n2 == 0:
+        return 0
+    else:
+        return n1 + n2
 
-multi = sc.parallelize(friendMat) \
-        .map(make_pair) \
-        .flatMap(matrix_multiple) \
-        .reduceByKey(lambda n1, n2: n1 + n2) \
-        .filter(lambda x: x[0][0] < x[0][1]) \
-        .filter(lambda x: friendMat[x[0][0]][x[0][1]] == 0) \
+pairs = split.flatMap(make_pair) \
+        .reduceByKey(count_common) \
+        .filter(lambda p: p[1] != 0) \
         .sortByKey()
 
-#print "This is multi"
-#print multi.collect()
-
-final = multi.map(lambda x: (x[1], x[0])) \
+final = pairs.map(lambda x: (x[1], x[0])) \
         .sortByKey(False) \
         .take(10)
 
@@ -69,6 +44,5 @@ final = multi.map(lambda x: (x[1], x[0])) \
 
 for i in final:
 	print "%s\t%s\t%d" % (i[1][0], i[1][1], i[0])
-
 
 sc.stop()
---
+++ hw1/test1_7.py
@@ -0,0 +1,80 @@
+import re
+import sys
+import numpy as np
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+lines = sc.textFile(sys.argv[1])
+
+
+split = lines.map(lambda l: re.split(r'\t', l)) \
+        .filter(lambda l: l[1] != '') \
+        .map(lambda l: (l[0], re.split(r',', l[1]))) \
+        .collect() 
+        #.collect()
+        #lines.map(lambda l: re.split(r'\t', l)) \
+        #.map(lambda s: (s[0], re.split(r',', s[1]))) \
+        #.collect()
+	    #.filter(lambda l: len(l) > 1) \
+        # #.filter(lambda s: len(s[1]) > 1) \
+
+#numPeople = int(max(max(split))) + 1
+numPeople = 50000
+
+#print "This is split"
+#print split.collect()
+
+#print "How many people"
+#print numPeople
+
+# Make path(friends) matrix
+friendMat = np.zeros((numPeople, numPeople))
+
+for i in split:
+    who =int(i[0])
+    friends = i[1]
+    for j in friends:
+        friendMat[who][int(j)] += 1
+
+#print "Matrix of friend"
+#print friendMat
+
+# make (index, value) pair
+def make_pair(x):
+    pairs = []
+    for i in range(len(x)):
+        pairs.append((i, int(x[i])))
+    return pairs
+
+# multiplication of symmetric matrix
+def matrix_multiple(x):
+    value = []
+    for i in x:
+        for j in x:
+            value.append(((i[0],j[0]), i[1]*j[1]))
+    return value
+
+multi = sc.parallelize(friendMat) \
+        .map(make_pair) \
+        .flatMap(matrix_multiple) \
+        .reduceByKey(lambda n1, n2: n1 + n2) \
+        .filter(lambda x: x[0][0] < x[0][1]) \
+        .filter(lambda x: friendMat[x[0][0]][x[0][1]] == 0) \
+        .sortByKey()
+
+#print "This is multi"
+#print multi.collect()
+
+final = multi.map(lambda x: (x[1], x[0])) \
+        .sortByKey(False) \
+        .take(10)
+
+#print "Final"
+#print final
+
+for i in final:
+	print "%s\t%s\t%d" % (i[1][0], i[1][1], i[0])
+
+
+sc.stop()
