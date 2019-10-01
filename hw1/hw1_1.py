1b4b75d11d5d2e546e5d9cc29ac46dceacadeb5f
sjuuun <osjuny11@gmail.com>
2019-10-01 14:04:52 +0900

    done matrix multiplication, but debug needed

--- hw1/hw1_1.py
+++ hw1/hw1_1.py
@@ -10,16 +10,18 @@ lines = sc.textFile(sys.argv[1])
 
 split = lines.map(lambda l: re.split('\t|,', l)) \
         .collect()
+        #.collect()
         #lines.map(lambda l: re.split(r'\t', l)) \
         #.map(lambda s: (s[0], re.split(r',', s[1]))) \
         #.collect()
 	    #.filter(lambda l: len(l) > 1) \
         # #.filter(lambda s: len(s[1]) > 1) \
-print split
-numPeople = len(split)
 
-print "How many people"
-print numPeople
+#numPeople = int(max(max(split))) + 1
+numPeople = 50000
+
+#print "How many people"
+#print numPeople
 
 # Make path(friends) matrix
 friendMat = np.zeros((numPeople, numPeople))
@@ -29,25 +31,44 @@ for i in range(numPeople):
     for j in friends:
         friendMat[i][int(j)] += 1
 
-print "Matrix of friend"
-print friendMat
+#print "Matrix of friend"
+#print friendMat
+
+# make (index, value) pair
+def make_pair(x):
+    pairs = []
+    for i in range(len(x)):
+        pairs.append((i, int(x[i])))
+    return pairs
+
+# multiplication of symmetric matrix
+def matrix_multiple(x):
+    value = []
+    for i in x:
+        for j in x:
+            value.append(((i[0],j[0]), i[1]*j[1]))
+    return value
 
 multi = sc.parallelize(friendMat) \
-        .collect()
+        .map(make_pair) \
+        .flatMap(matrix_multiple) \
+        .reduceByKey(lambda n1, n2: n1 + n2) \
+        .filter(lambda x: x[0][0] < x[0][1]) \
+        .filter(lambda x: friendMat[x[0][0]][x[0][1]] == 0) \
+        .sortByKey()
 
-print "This is multi"
-print multi
-"""
-pairs = split.flatMap(make_pair) \
-	.collect()
+#print "This is multi"
+#print multi.collect()
 
-
-final = interest.map(lambda i: (i[1], i[0])) \
+final = multi.map(lambda x: (x[1], x[0])) \
         .sortByKey(False) \
         .take(10)
 
+#print "Final"
+#print final
+
 for i in final:
 	print "%s\t%s\t%d" % (i[1][0], i[1][1], i[0])
-"""
+
 
 sc.stop()
