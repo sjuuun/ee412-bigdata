bce24a7e3f66bcdced2414b5fc854ae694b3ffd1
sjuuun <osjuny11@gmail.com>
2019-10-27 17:02:15 +0900

    SVD eigenvector sign issue

--- hw2/hw2_2b.py
+++ hw2/hw2_2b.py
@@ -12,10 +12,52 @@ print MMT
 
 # (b) Compute eigenpairs of MTM and MMT.
 print "eigenpair of MTM"
-print np.linalg.eig(MTM)
+MTM_pair = np.linalg.eig(MTM)
+print MTM_pair
 print "eigenpair of MMT"
-print np.linalg.eig(MMT)
+MMT_pair = np.linalg.eig(MMT)
+print MMT_pair
 
-# (c) Find SVD
-Sig = []
+# (c) Find SVD (assume all eigenvalues are different)
+rank = np.count_nonzero(MTM_pair[0].round(5))
+print "This is rank"
+print rank
 
+eig_values = []
+V = np.zeros((M.shape[1], rank))
+U = np.zeros((M.shape[0], rank))
+values = list(MTM_pair[0])
+
+#print MTM_pair[1]
+# Find V, which is matrix of eigenvectors of MTM
+for i in range(rank):
+    index = values.index(max(values))
+    eig_values.append(values[index])
+    values[index] = np.NINF
+    V[:,i] = -MTM_pair[1][:,index]
+    '''
+    if MTM_pair[1][0,index] < 0:
+        V[:,i] = -MTM_pair[1][:,index]
+    else:
+        V[:,i] = MTM_pair[1][:,index]
+    '''
+print V
+
+# Find U, which is matrix of eigenvectors of MMT
+values = list(MMT_pair[0])
+for i in range(rank):
+    index = values.index(max(values))
+    values[index] = np.NINF
+    U[:,i] = MMT_pair[1][:,index]
+    '''
+    if MMT_pair[1][0,index] < 0:
+        U[:,i] = -MMT_pair[1][:,index]
+    else:
+        U[:,i] = MMT_pair[1][:,index]
+    '''
+print U
+
+sigma = np.sqrt(np.diag(eig_values))
+print sigma
+#print np.dot(U,sigma)
+print np.dot(np.dot(U,sigma), np.transpose(V))
