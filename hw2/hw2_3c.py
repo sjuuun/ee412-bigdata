9991d4717d3c4093d77e5aac44f43f7d344b32ef
sjuuun <osjuny11@gmail.com>
2019-10-31 01:12:17 +0900

    hw2_3c: Construct utility matrix

--- hw2/hw2_3c.py
+++ hw2/hw2_3c.py
@@ -6,18 +6,40 @@ import numpy as np
 f = open(sys.argv[1], 'r')  # ratings.txt
 q = open(sys.argv[2], 'r')  # ratings_test.txt
 
-lines = f.readlines()
+linesF = f.readlines()
+linesQ = q.readlines()
 pairs = []
 user_list = []
 item_list = []
 
-for line in lines:
+# Input from ratings.txt
+for line in linesF:
     tmp = re.split(',', line)[:-1]
     pairs.append([int(tmp[0]), int(tmp[1]), float(tmp[2])])
     user_list.append(int(tmp[0]))
     item_list.append(int(tmp[1]))
+
+# Input from ratings_test.txt
+for line in linesQ:
+    tmp = re.split(',', line)
+    user_list.append(int(tmp[0]))
+    item_list.append(int(tmp[1]))
+
 # Make distinct list
 user_list = list(set(user_list))
 item_list = list(set(item_list))
-users = max(user_list)
-items = max(item_list)
+users = len(user_list)
+items = len(item_list)
+
+user_dic = {}
+item_dic = {}
+for i in range(users):
+    user_dic[user_list[i]] = i
+for i in range(items):
+    item_dic[item_list[i]] = i
+
+util_matrix = np.zeros((users, items))
+norm_matrix = np.zeros((users, items))
+
+for pair in pairs:
+    util_matrix[user_dic[pair[0]], item_dic[pair[1]]] = pair[2]
