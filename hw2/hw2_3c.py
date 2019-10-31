9de8b1aec5152b0b352686f2056f58d829da8c95
sjuuun <osjuny11@gmail.com>
2019-10-31 21:41:49 +0900

    Might complete hw2_3c

--- hw2/hw2_3c.py
+++ hw2/hw2_3c.py
@@ -9,9 +9,10 @@ q = open(sys.argv[2], 'r')  # ratings_test.txt
 
 linesF = f.readlines()
 linesQ = q.readlines()
-pairs = []
-user_list = []
-item_list = []
+pairs = []                  # user and item pair of ratings.txt
+test_pairs = []             # user and item pair of ratings_test.txt
+user_list = []              # list of users
+item_list = []              # list of items
 
 # Input from ratings.txt
 for line in linesF:
@@ -25,6 +26,7 @@ for line in linesQ:
     tmp = re.split(',', line)
     user_list.append(int(tmp[0]))
     item_list.append(int(tmp[1]))
+    test_pairs.append(tmp)
 
 # Make distinct list
 user_list = list(set(user_list))
@@ -32,6 +34,8 @@ item_list = list(set(item_list))
 users = len(user_list)
 items = len(item_list)
 
+# Make dictionary of user and item
+# Its key is ID of user or item, and value is index of user of item in util_matrix
 user_dic = {}
 item_dic = {}
 for i in range(users):
@@ -44,8 +48,6 @@ norm_matrix = np.zeros((users, items))
 
 for pair in pairs:
     util_matrix[user_dic[pair[0]], item_dic[pair[1]]] = pair[2]
-
-#print (np.linalg.matrix_rank(util_matrix))
 
 # Noramlize util_matrix to norm_matrix
 user_avg = np.zeros(users)
@@ -60,24 +62,20 @@ for i in range(users):
     user_std[i] = std
     user_vector[nonzero_index] = (user_vector[nonzero_index] - avg) / std
     norm_matrix[i,:] = user_vector
-'''
-print np.shape(norm_matrix)
-U,S,Vt = np.linalg.svd(norm_matrix, full_matrices=False)
-print np.shape(U)
-print np.shape(S)
-print np.shape(Vt)
 
-#pred_matrix = np.dot(np.dot(U[:,:-1], np.diag(S[:-1])), Vt[:-1,:])
-pred_matrix = np.dot(U, Vt)
-
-print rmse(norm_matrix, pred_matrix)
-'''
+# Compute rmse of a and b
 def rmse(a,b):
     if not np.any(a-b):
         return 0
     return np.sqrt(np.mean(np.square(np.array(a)-np.array(b))))
 
+# Compute square sum of a-b (used in optimizing)
+def error(a,b):
+    return np.sum(np.square(a-b))
+
 # Optimize U[i,j]
+# M is norm_matrix, U is U matrix, V is V matrix
+# Here, we optimize U[i,j]
 def optimize_u(M, U, V, i, j):
     base = M[i,:]
     target = U[i,:]
@@ -85,18 +83,22 @@ def optimize_u(M, U, V, i, j):
     start = -0.5
     d = 0.1
     target[j] += start
-    err = rmse(base[nonzero_index], np.dot(target,V)[nonzero_index])
+    err = error(base[nonzero_index], np.dot(target,V)[nonzero_index])
     for x in range(3):
         for _ in range(10):
             target[j] += d
-            tmp = rmse(base[nonzero_index], np.dot(target,V)[nonzero_index])
+            tmp = error(base[nonzero_index], np.dot(target,V)[nonzero_index])
             if tmp > err:
-                target [j] -= d
+                target[j] -= d
                 break
             err = tmp
         d *= 0.1
+        start *= 0.1
+        target[j] += start
 
 # Optimize U[i,j]
+# M is norm_matrix, U is U matrix, V is V matrix
+# Here, we optimize V[i,j]
 def optimize_v(M, U, V, i, j):
     base = M[:,j]
     target = V[:,j]
@@ -104,32 +106,60 @@ def optimize_v(M, U, V, i, j):
     start = -0.5
     d = 0.1
     target[i] += start
-    err = rmse(base[nonzero_index], np.dot(U,target)[nonzero_index])
+    err = error(base[nonzero_index], np.dot(U,target)[nonzero_index])
     for x in range(3):
         for _ in range(10):
             target[i] += d
-            tmp = rmse(base[nonzero_index], np.dot(target,V)[nonzero_index])
+            tmp = error(base[nonzero_index], np.dot(U,target)[nonzero_index])
             if tmp > err:
-                target [i] -= d
+                target[i] -= d
                 break
             err = tmp
         d *= 0.1
+        start *= 0.1
+        target[i] += start
 
-U = np.zeros((users, 100))
-V = np.zeros((100, items))
+# Make U and V matrices
+rank = 30
+U = np.zeros((users, rank))
+V = np.zeros((rank, items))
+
+# Make train_pairs
+# We will train U and V matrices with this pairs
+# 0 means optimize with U, 1 means optimize V
 train_pairs = []
 for i in range(users):
-    for j in range(items):
-        if (util_matrix[i,j] == 0):
-            continue
-        train_pairs.append((i,j))
-random.shuffle(train_pairs)
+    for j in range(rank):
+        train_pairs.append((0,i,j))
+for i in range(items):
+    for j in range(rank):
+        train_pairs.append((1,j,i))
+#random.shuffle(train_pairs)
 
-for x in range(100):
-    for i,j in train_pairs:
-        optimize_u(norm_matrix,U,V,i,x)
-        optimize_v(norm_matrix,U,V,x,j)
-    print x
-    print rmse(norm_matrix, np.dot(U,V))
+# Optimize UV decomposition
+print len(train_pairs)
+count = 0
+for _ in range(1):
+    random.shuffle(train_pairs)
+    for x,i,j in train_pairs:
+        if x == 0:
+            optimize_u(norm_matrix,U,V,i,j)
+        else:
+            optimize_v(norm_matrix,U,V,i,j)
+        count += 1
+        if count % 10000 == 0:
+            print count
 
 print rmse(norm_matrix, np.dot(U,V))
+print U
+
+pred_matrix = np.dot(U,V)
+# Write Result file
+w = open("output.txt", 'w')
+for pair in test_pairs:
+    pred_user = int(pair[0])
+    pred_item = int(pair[1])
+    u_index = user_dic[pred_user]
+    i_index = item_dic[pred_item]
+    pred_rate = (norm_matrix[u_index, i_index] * user_std[u_index]) + user_avg[u_index]
+    w.write(pair[0] + "," + pair[1] + "," + str(pred_rate) + "," + pair[-1])
