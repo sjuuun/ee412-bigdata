0124a265bf00a4d3c29c660c148c5050fae48956
sjuuun <osjuny11@gmail.com>
2019-11-07 14:06:00 +0900

    Complete hw3_1

--- hw3/hw3_1.py
+++ hw3/hw3_1.py
@@ -3,7 +3,54 @@ import sys
 from pyspark import SparkConf, SparkContext
 
 # Get imput
-f = open(sys.argv[1], 'r')
-
+conf = SparkConf()
+sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1])
 
+# Split and mapping of transition matrix M
+# (i,j) means Mji = 1
+M = lines.map(lambda l: list(map(int, l.split())))
+
+# Count how many times source appear
+source = M.map(lambda pair: (pair[0], 1)) \
+          .reduceByKey(lambda n1, n2: n1+n2) \
+          .collect()
+s_dic = {}
+for s in source:
+    s_dic[s[0]] = s[1]
+
+# Initialize vector v, e, beta
+v = [1/len(s_dic)] * len(s_dic)
+beta = 0.9
+e = (1-beta)/len(s_dic)
+
+# Mapping in matrix multiplication
+def multiple(pair):
+    c, r = pair
+    return (r, v[c-1]/s_dic[c])
+    #p[1], v[p[0]-1]/s_dic[p[0]]
+
+# Texation: beta*M*v + (1-beta)*e/n
+def texation(value):
+    return beta*value + e
+
+# Iterate 50 times
+iter = 50
+for _ in range(iter):
+    mul = M.map(multiple) \
+            .reduceByKey(lambda n1, n2: n1+n2) \
+            .collect()
+    # Sort ascending order according to row number
+    mul.sort(key = lambda x: x[0])
+    # Make new v
+    v = [texation(x[1]) for x in mul]
+
+# Result
+# Question: how do we break tie?
+pr = []
+for i in range(len(v)):
+    pr.append((i+1, v[i]))
+pr.sort(key = lambda x: -x[1])
+pr = pr[:10]
+for top in pr:
+    print("%d\t%.10f" % (top[0], top[1]))
