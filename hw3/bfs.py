046b0f42a36ed58c0506677ab2f3d03249e7ef99
sjuuun <osjuny11@gmail.com>
2019-11-21 21:44:26 +0900

    Complete bfs algorithm

--- hw3/bfs.py
+++ hw3/bfs.py
@@ -17,57 +17,76 @@ for adj in adjList:
 print adjPoint
 
 # Define class
-'''
-class BFS:
-    def __init__ (self, point):
-        self.src = point
-        self.dst = point
-        self.distance = 0
-        self.weight = 1
-        self.path = []
-        self.adjacent = adjPoint[point]
-    def print_bfs(self):
-        print "src: %d, dst: %d, dis: %d, weight: %d" % (self.src, self.dst, self.distance, self.weight)
-        print self.path
-        print self.adjacent
-    def map(self):
-        return ((self.src, self.dst), self)
-'''
-DIST = 0
-WEIGHT = 1
-PATH = 2
-ADJACENT = 3
+DONE = 0
+DIST = 1
+WEIGHT = 2
+PATH = 3
 
-def bfs_init(point):
-    return ((point, point), [0,1,[],adjPoint[point]])
+def bfs_init((src, dst)):
+    return ((src, dst), [0,1,1,[[src]]])
+
+def bfs_left((K, V)):
+    return not V[DONE]
 
 def bfs_adj(path, adj):
-    return [p for p in adj if not(p in path)]
+    flatten = [y for x in path for y in x]
+    flatten = list(set(flatten))
+    return [p for p in adj if not(p in flatten)]
 
 def bfs_next(data, target):
-    (src, dst), [dis, weight, path, adj] = data
-    assert (target in adj)
-    dis += 1
-    path = path + [dst]
-    adj = bfs_adj(path, adjPoint[target])
-    return ((src, target), [dis, weight, path, adj])
+    (src, dst), [done, dist, weight, path] = data
+    dist += 1
+    path = [p + [dst] for p in path]
+    return ((src, target), [done, dist, weight, path])
 
 def bfs_map(data):
-    if len(data[1][ADJACENT]) == 0:
+    K,V = data
+    if V[DONE]:
         return [data]
     else:
-        result = [bfs_next(data, p) for p in data[1][ADJACENT]]
-        data[1][ADJACENT] = []
+        adj = bfs_adj(V[PATH], adjPoint[K[1]])
+        result = [bfs_next(data, p) for p in adj]
+        V[DONE] = 1
         return [data] + result
 
-start = pair.keys().distinct() \
-            .map(lambda k: bfs_init(k))
+def bfs_reduce(V1, V2):
+    if V1[DIST] < V2[DIST]:
+        return V1
+    elif V1[DIST] > V2[DIST]:
+        return V2
+    else:
+        assert(V1[DIST] == V2[DIST])
+        w = V1[WEIGHT] + V2[WEIGHT]
+        path = V1[PATH] + V2[PATH]
+        return [V1[0], V1[1], w, path]
+
+def bfs_edge(path, dst):
+    edge = []
+    for i in range(len(path)-1):
+        edge.append((path[i], path[i+1]))
+    edge.append((path[-1], dst))
+    return edge
+
+def bfs_between((K,V)):
+    w = 1 / float(V[WEIGHT])
+    edge = []
+    for p in V[PATH]:
+        edge = edge + bfs_edge(p, K[1])
+    return [(e,w) for e in edge]
 
 
-bfs = start.flatMap(bfs_map)
-have = bfs.map(lambda d: d[0]).distinct().collect()
-print have
-#for s in start.collect():
-#    print s
+bfs = pair.map(bfs_init)
+print bfs.filter(bfs_left).count()
+while (bfs.filter(bfs_left).count() > 0):
+    bfs = bfs.flatMap(bfs_map) \
+             .reduceByKey(bfs_reduce)
+
 for b in bfs.collect():
     print b
+
+# Step 3
+between  = bfs.flatMap(bfs_between) \
+              .filter(lambda k: k[0][0] < k[0][1]) \
+              .reduceByKey(lambda n1,n2: n1+n2) \
+              .collect()
+print between
