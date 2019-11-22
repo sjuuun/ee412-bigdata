6e38e065b801ea0697ca59ea9976f69d54a9bb28
sjuuun <osjuny11@gmail.com>
2019-11-23 01:43:06 +0900

    For submit

--- hw3/hw1a.py
+++ hw3/hw1a.py
@@ -1,10 +1,18 @@
 import numpy as np
 
+'''
 M = np.array([1/3, 1/2, 0, 1/3, 0, 1/2, 1/3, 1/2, 1/2]).reshape((3,3))
 print(M)
 
 M = np.identity(3) - 0.8*M
 e = 0.2*np.array([1/3, 1/3, 1/3]).reshape((3,1))
-#e = 0.2*np.array([1, 0, 0]).reshape((3,1))
-#e = 0.2*np.array([1/2, 0, 1/2]).reshape((3,1))
+print(np.dot(np.linalg.inv(M), e))
+'''
+
+M = np.array([0, 1/2, 1, 0, 1/3, 0, 0, 1/2, 1/3, 0, 0, 1/2, 1/3, 1/2, 0, 0]).reshape((4,4))
+print (M)
+
+M = np.identity(4) - 0.8*M
+#e = 0.2*np.array([1, 0, 0, 0]).reshape((4,1))      # For a: A only
+e = 0.2*np.array([1/2, 0, 1/2, 0]).reshape((4,1))   # For b: A and C
 print(np.dot(np.linalg.inv(M), e))
--- hw3/hw3_2_p2.py
+++ hw3/hw3_2_p2.py
@@ -7,13 +7,10 @@ def author_group(group):
         result.append((group[i], group[:i] + group[(i+1):]))
     return result
 
-def update_hitPoint(hitPoint, group):
-    for g in group:
-        src, hit = g
-        hitPoint[src] += [h[0] for h in hit]
-
 def GN_per_root(root):
+    # Node information: (distance, weight)
     node = {root:(0,1)}
+    # Dictionary: parents of node(used in step3)
     parent = {}
     after = [root]
     cur = []
@@ -21,6 +18,7 @@ def GN_per_root(root):
     while(len(after) != 0):
         cur = after
         after = []
+        # Temporal node information
         tmpNode = {}
         for c in cur:
             tmp = [p for p in adjPoint[c] if not(p in node)]
@@ -36,20 +34,22 @@ def GN_per_root(root):
                     parent[t].add(c)
                 else:
                     parent[t] = set([c])
+            
             after += tmp
+
         node.update(tmpNode)
-        cur = list(set(after))
         after = list(set(after))
 
     # Step 3: compute edge weight
     allnode = list(node)
-    allnode.sort(key = lambda n: -node[n][0])
+    allnode.sort(key = lambda n: -node[n][0]) # start from leaf
+    # Dictionary for node and weighted-value: (node, value)
     nodeWeight = {}
     result = []
     for a in allnode[:-1]:
         if nodeWeight.get(a):
             my = nodeWeight[a] + 1
-        else:
+        else: # leaf
             my = 1
         for p in list(parent[a]):
             parentWeight = (my*node[p][1]) / float(node[a][1])
@@ -58,11 +58,13 @@ def GN_per_root(root):
                 result.append(((p, a), parentWeight))
             else:
                 result.append(((a, p), parentWeight))
+            
             # Update parent's node weight
             if nodeWeight.get(p):
                 nodeWeight[p] += parentWeight
             else:
                 nodeWeight[p] = parentWeight
+
     return result
 
 if __name__=="__main__":
@@ -78,18 +80,20 @@ if __name__=="__main__":
                 .map(lambda line: list(map(int, line.split(','))) ) \
                 .map(lambda pair: (pair[1], [pair[2]])) \
                 .reduceByKey(lambda n1, n2: n1+n2) \
-                .map(lambda group: group[1])
+                .map(lambda group: group[1]) \
+                .filter(lambda k: len(k) > 1)
 
     adjPoint = pairs.flatMap(author_group) \
                     .reduceByKey(lambda n1, n2 : list(set(n1+n2))) \
                     .collectAsMap()
-
-    gn = pairs.flatMap(lambda group: group) \
-                .distinct() \
+    
+    #gn = pairs.flatMap(lambda group: group) \
+    gn = sc.parallelize(adjPoint) \
                 .flatMap(GN_per_root) \
                 .reduceByKey(lambda n1, n2: n1+n2) \
                 .collect()
-    
+                                #.distinct() \
+
     gn.sort(key = lambda x: -x[1])
     for g in gn[:10]:
         print ("%d\t%d\t%.5f" % (g[0][0], g[0][1], g[1]/2))
